15682b7e819996c60fd905ed8105311af47573c5
maruzzing <sseulsseul@gmail.com>
2019-05-31 15:13:12 +0900

    startPage revised

--- App.js
+++ App.js
@@ -11,21 +11,12 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 // import { Provider } from 'react-redux';
 // import { store } from './src/store';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
 // type Props = {};
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>fetch!</Text>
       </View>
     );
   }
@@ -37,15 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
   }
 });
